4f49f5b2a232393377dafc49f4f14132cda8a28a
Jorge Díaz <jlditru@gmail.com>
2016-06-19 23:50:44 -0500

    Inyección de dependencias con Butterknife Zelezny

--- Leccion0_Fundamentos_Basicos/app/src/main/java/com/co/joluditru/tipcalc/MainActivity.java
+++ Leccion0_Fundamentos_Basicos/app/src/main/java/com/co/joluditru/tipcalc/MainActivity.java
@@ -1,18 +1,49 @@
 package com.co.joluditru.tipcalc;
 
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    @BindView(R.id.input_bill)
+    EditText inputBill;
+    @BindView(R.id.btn_submit)
+    Button btnSubmit;
+    @BindView(R.id.input_percentage)
+    EditText inputPercentage;
+    @BindView(R.id.btn_increase)
+    Button btnIncrease;
+    @BindView(R.id.btn_decrease)
+    Button btnDecrease;
+    @BindView(R.id.btn_clear)
+    Button btnClear;
+    @BindView(R.id.lbl_tip)
+    TextView lblTip;
+
+    private final static int TIP_CHANGE = 1;
+    private final static int DEFAULT_TIP_PERCENTAGE = 10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 
     @Override
@@ -23,7 +54,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == R.id.action_about){
+        if (item.getItemId() == R.id.action_about) {
             about();
         }
         return super.onOptionsItemSelected(item);
@@ -36,5 +67,31 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(Intent.ACTION_VIEW);
         intent.setData(Uri.parse(strUrl));
         startActivity(intent);
+    }
+
+    @OnClick({R.id.btn_submit, R.id.btn_increase, R.id.btn_decrease, R.id.btn_clear})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_submit:
+                hideKeyBoard();
+                break;
+            case R.id.btn_increase:
+                break;
+            case R.id.btn_decrease:
+                break;
+            case R.id.btn_clear:
+                break;
+        }
+    }
+
+    private void hideKeyBoard() {
+        InputMethodManager inputMethodManager = (InputMethodManager)
+                getSystemService(Context.INPUT_METHOD_SERVICE);
+        try{
+            inputMethodManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),
+                        InputMethodManager.HIDE_NOT_ALWAYS);
+        }catch (NullPointerException npe){
+            Log.e(getLocalClassName(), Log.getStackTraceString(npe));
+        }
     }
 }
