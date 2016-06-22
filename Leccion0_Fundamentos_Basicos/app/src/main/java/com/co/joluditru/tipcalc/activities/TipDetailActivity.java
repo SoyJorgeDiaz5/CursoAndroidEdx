154f505aa8bbb6d2e77f138411f7afcea4c30fa4
Jorge Díaz <jlditru@gmail.com>
2016-06-22 00:55:54 -0500

    Diseño del detalle de la propina

---
+++ Leccion0_Fundamentos_Basicos/app/src/main/java/com/co/joluditru/tipcalc/activities/TipDetailActivity.java
@@ -0,0 +1,15 @@
+package com.co.joluditru.tipcalc.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.co.joluditru.tipcalc.R;
+
+public class TipDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tip_detail);
+    }
+}
