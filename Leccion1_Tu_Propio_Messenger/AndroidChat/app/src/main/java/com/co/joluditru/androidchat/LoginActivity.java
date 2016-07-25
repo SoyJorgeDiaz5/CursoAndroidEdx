6be0e6d85ed38210612ec655bd20f449daed19fb
Jorge Díaz <jlditru@gmail.com>
2016-07-24 23:32:37 -0500

    Agregando Butterknife

--- Leccion1_Tu_Propio_Messenger/AndroidChat/app/src/main/java/com/co/joluditru/androidchat/LoginActivity.java
+++ Leccion1_Tu_Propio_Messenger/AndroidChat/app/src/main/java/com/co/joluditru/androidchat/LoginActivity.java
@@ -2,12 +2,47 @@ package com.co.joluditru.androidchat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class LoginActivity extends AppCompatActivity {
+
+    @Bind(R.id.txt_email)
+    EditText inputEmail;
+    @Bind(R.id.txt_password)
+    EditText inputPassword;
+    @Bind(R.id.progressBar)
+    ProgressBar progressBar;
+    @Bind(R.id.layout_main_container)
+    RelativeLayout container;
+    @Bind(R.id.btn_signin)
+    Button btnSignin;
+    @Bind(R.id.btn_signup)
+    Button btnSignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
     }
+
+    @OnClick(R.id.btn_signin)
+    public void handleSignin(){
+        Log.e("ANDROID-CHAT",inputEmail.getText().toString());
+    }
+
+    @OnClick(R.id.btn_signup)
+    public void handleSignup(){
+        Log.e("ANDROID-CHAT",inputEmail.getText().toString());
+    }
+
 }
