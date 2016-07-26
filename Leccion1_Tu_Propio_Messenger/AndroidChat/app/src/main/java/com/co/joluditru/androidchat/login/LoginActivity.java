bc98a217ae5106ca97f3991c8e983755fcbf63be
Jorge Díaz <jlditru@gmail.com>
2016-07-25 23:46:04 -0500

    Implementación de Presenter

--- Leccion1_Tu_Propio_Messenger/AndroidChat/app/src/main/java/com/co/joluditru/androidchat/login/LoginActivity.java
+++ Leccion1_Tu_Propio_Messenger/AndroidChat/app/src/main/java/com/co/joluditru/androidchat/login/LoginActivity.java
@@ -1,21 +1,23 @@
 package com.co.joluditru.androidchat.login;
 
+import android.content.Intent;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Layout;
-import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 
 import com.co.joluditru.androidchat.R;
+import com.co.joluditru.androidchat.contactlist.ContactListActivity;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements LoginView{
 
     @Bind(R.id.txt_email)
     EditText inputEmail;
@@ -30,6 +32,8 @@ public class LoginActivity extends AppCompatActivity {
     @Bind(R.id.btn_signup)
     Button btnSignup;
 
+    private LoginPresenter loginPresenter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,14 +41,68 @@ public class LoginActivity extends AppCompatActivity {
         ButterKnife.bind(this);
     }
 
+    @Override
+    public void enableInputs() {
+        setInputs(true);
+    }
+
+    @Override
+    public void disableInputs() {
+        setInputs(false);
+    }
+
+    @Override
+    public void showProgress() {
+        progressBar.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void hideProgress() {
+        progressBar.setVisibility(View.GONE);
+    }
+
     @OnClick(R.id.btn_signin)
-    public void handleSignin(){
-        Log.e("ANDROID-CHAT",inputEmail.getText().toString());
+    @Override
+    public void handleSignIn() {
+        loginPresenter.validateLogin(inputEmail.getText().toString(),
+                inputPassword.getText().toString());
     }
 
     @OnClick(R.id.btn_signup)
-    public void handleSignup(){
-        Log.e("ANDROID-CHAT",inputEmail.getText().toString());
+    @Override
+    public void handleSignUp() {
+        loginPresenter.registerNewUser(inputEmail.getText().toString(),
+                inputPassword.getText().toString());
     }
 
+    @Override
+    public void navigateToMainScreen() {
+        startActivity(new Intent(this, ContactListActivity.class));
+    }
+
+    @Override
+    public void loginError(String error) {
+        inputPassword.setText("");
+        String msgError = String.format(getString(R.string.login_error_message_signin), error);
+        inputPassword.setError(msgError);
+    }
+
+    @Override
+    public void newUserSuccess() {
+        Snackbar.make(container, R.string.login_notice_message_signup, Snackbar.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void newUserError(String error) {
+        inputPassword.setText("");
+        String msgError = String.format(getString(R.string.login_error_message_signup), error);
+        inputPassword.setError(msgError);
+    }
+
+    private void setInputs(boolean enabled){
+        inputEmail.setEnabled(enabled);
+        inputPassword.setEnabled(enabled);
+        btnSignin.setEnabled(enabled);
+        btnSignup.setEnabled(enabled);
+    }
 }
